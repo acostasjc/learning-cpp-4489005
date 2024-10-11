9584e2debb650dd33cafccf6d9baa2603aa9e9ae
acostasjc <111208548+acostasjc@users.noreply.github.com>
2024-10-11 17:17:25 +0000

    02_11

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -3,10 +3,31 @@
 // Type Casting Examples, by Eduardo Corpeño 
 
 #include <iostream>
-#include <cstdint>
+
 
 int main(){
     
+    int fahrenheit = 100;
+    float celsius; 
+    
+    celsius = (5.0f/9)*(fahrenheit - 32);
+
+    std::cout << std::endl;
+
+    std::cout << "Fahrenheit: " << fahrenheit << std::endl;
+    std::cout << "Celsius   : " << celsius << std::endl;
+    
+    double weight = 10.99;
+
+    std::cout << std::endl;
+
+    std::cout << "          Double: " << weight << std::endl;
+    std::cout << "   Integer Part: " << (int) weight << std::endl;
+    std::cout << "Fractional Part: " << (int)((weight - (int)weight) * 10000) << std::endl;
+    
+
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
