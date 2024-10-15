7af2710aeb953965204910916db5989a5f62a539
acostasjc <111208548+acostasjc@users.noreply.github.com>
2024-10-15 13:10:41 +0000

    programa 14

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -1,12 +1,32 @@
-// Learning C++ 
-// Exercise 03_06
-// Pointers, by Eduardo Corpeño 
-
+// PROGRAMA 14
+ 
 #include <iostream>
-#include <string>
-
+ 
+ 
 int main(){
-
+ 
+    float num_2;
+ 
+    int a = 37;
+    int *ptr;
+ 
+    ptr = &a;
+ 
+    std::cout << "The content of a is " << a << std::endl;
+    std::cout << "ptr is pointing to address " << ptr << std::endl;
+    std::cout << "The address of a is  " << &a << std::endl;
+    std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
+    std::cout << "The address of ptr is " << &ptr << std::endl;
+ 
+    //std::cout << "sizeof de int " << sizeof(long int) << std::endl;
+   
+   
+   
+ 
+ 
+    std::cout << std::endl << "Pressione um numero e enter, para sair... ";
+    std::cin >> num_2;
+ 
     std::cout << std::endl << std::endl;
     return (0);
 }
