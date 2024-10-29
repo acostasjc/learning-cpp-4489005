ac80e8386ba481a243479484acd3a84d5299583c
acostasjc <111208548+acostasjc@users.noreply.github.com>
2024-10-29 14:14:11 +0000

    PROGRAMA 21A

--- src/Ch04/04_05/CodeDemo.cpp
+++ src/Ch04/04_05/CodeDemo.cpp
@@ -21,17 +21,45 @@ int main(){
     std::vector<Grade> grades = {Grade(1, 1, 'B'), Grade(1, 2, 'A'), Grade(1, 3, 'C'),
                                 Grade(2, 1, 'A'), Grade(2, 2, 'A'), Grade(2, 4, 'B')};
 
-    std::cout << "Enter a student ID: " << std::flush;
-    std::cin >> id;
+    //std::cout << "Enter a student ID: " << std::flush;
+    //std::cin >> id;
 
     // Calculate the GPA for the selected student.
     // Write your code here
+
+
 
     std::string student_str;
     student_str = students[0].get_name(); // Change this to the selected student's name
 
     std::cout << "The GPA for " << student_str << " is " << GPA << std::endl;
     
+    //std::cout << grades[0].get_grade() << std::endl;
+    std::cout << (int) (('C' - grades[0].get_grade())+2) << std::endl;
+    std::cout << courses[grades[0].get_course_id()-1].get_credits() << std::endl;
+    
+    std::cout << (int) (('C' - grades[1].get_grade())+2) << std::endl;
+    std::cout << courses[grades[1].get_course_id()-1].get_credits() << std::endl;
+
+    std::cout << (int) (('C' - grades[2].get_grade())+2) << std::endl;
+    std::cout << courses[grades[2].get_course_id()-1].get_credits() << std::endl;
+
+    int TotalCredits = 0;
+
+    for (int i = 0; i < 3; i++){
+        GPA += ((int) (('C' - grades[i].get_grade())+2))*courses[grades[i].get_course_id()-1].get_credits();
+    
+        TotalCredits += courses[grades[i].get_course_id()-1].get_credits();
+
+    }
+
+   
+    GPA /= TotalCredits;
+
+
+
+    std::cout << GPA << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
