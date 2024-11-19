8daf4859c2d611312b4012ebd9093418f1a14d5c
acostasjc <111208548+acostasjc@users.noreply.github.com>
2024-11-19 16:24:11 +0000

    PROGRAMA 28

--- src/Ch06/06_01b/CodeDemo.cpp
+++ src/Ch06/06_01b/CodeDemo.cpp
@@ -1,12 +1,17 @@
 // Learning C++ 
 // Exercise 06_01
+// PROGRAMA 28
 // Opening a text file for reading, by Eduardo Corpeño 
+
 
 #include <iostream>
 #include <string>
 #include <fstream>
 
+
+
 int main(){
+    
     std::ifstream inFile;
     std::string str;
     int number;
@@ -14,15 +19,25 @@ int main(){
 
     inFile.open("people.txt");
     if (inFile.fail())
+    {
         std::cout << std::endl << "File not found!" << std::endl;
-    else{
-        while (!inFile.eof()){
-            getline(inFile, str);
-            std::cout << str << std::endl;
+    }
+    else
+    {
+        while (!inFile.eof())
+        {
+            getline(inFile,str);
+            std::cout << str << ", ";
+            getline(inFile,str);
+            number = stoi(str);
+            std::cout << number << ", ";
+            getline(inFile,str);
+            letter = str[0];
+            std::cout << letter << std::endl;
         }
         inFile.close();
-    }
-    
+    } 
+
     std::cout << std::endl << std::endl;
     return (0);
 }
